4b6ed60737b34e73d5d60706d2b2d72ec8cef229
wimpi <wimpi@7c3eb096-4aa5-4855-946f-3e0195d27c68>
2012-08-11 19:26:35 +0000

    Enhancement of implementation design flaw as indicated by #3556408.
    git-svn-id: https://jamod.svn.sourceforge.net/svnroot/jamod/trunk@25 7c3eb096-4aa5-4855-946f-3e0195d27c68

--- src/main/java/net/wimpi/modbus/net/ModbusSerialListener.java
+++ src/main/java/net/wimpi/modbus/net/ModbusSerialListener.java
@@ -32,12 +32,13 @@ import net.wimpi.modbus.util.SerialParameters;
  * @author Dieter Wimberger
  * @version @version@ (@date@)
  */
-public class ModbusSerialListener {
+public class ModbusSerialListener
+    implements Runnable {
 
   //Members
-  private boolean m_Listening;               	//Flag for toggling listening/!listening
+  private boolean m_Listening;
   private SerialConnection m_SerialCon;
-  private static int c_RequestCounter = 0;          //counter for amount of requests
+  private Thread m_Listener;
 
   /**
    * Constructs a new <tt>ModbusSerialListener</tt> instance.
@@ -46,50 +47,65 @@ public class ModbusSerialListener {
    */
   public ModbusSerialListener(SerialParameters params) {
     m_SerialCon = new SerialConnection(params);
-    //System.out.println("Created connection.");
-    listen();
   }//constructor
 
   /**
+   * Starts this <tt>ModbusTCPListener</tt>.
+   */
+  public void start() {
+    m_Listener = new Thread(this);
+    m_Listener.start();
+    m_Listening = true;
+  }//start
+
+  /**
+   * Stops this <tt>ModbusTCPListener</tt>.
+   */
+  public void stop() {
+    m_Listening = false;
+    try {
+      m_Listener.join();
+    } catch (Exception ex) {
+      //
+    }
+  }//stop
+
+  /**
+   * Runs this <tt>ModbusSerialListener</tt>.
    * Listen to incoming messages.
    */
-  private void listen() {
+  public void run() {
     try {
       m_Listening = true;
       m_SerialCon.open();
       //System.out.println("Opened Serial connection.");
       ModbusTransport transport = m_SerialCon.getModbusTransport();
       do {
-        if (m_Listening) {
-          try {
-            //1. read the request
-            ModbusRequest request = transport.readRequest();
-            ModbusResponse response = null;
+        try {
+          //1. read the request
+          ModbusRequest request = transport.readRequest();
+          ModbusResponse response = null;
 
-            //test if Process image exists
-            if (ModbusCoupler.getReference().getProcessImage() == null) {
-              response =
-                  request.createExceptionResponse(Modbus.ILLEGAL_FUNCTION_EXCEPTION);
-            } else {
-              response = request.createResponse();
-            }
-
-            if (Modbus.debug)
-              System.out.println("Request:" + request.getHexMessage());
-            if (Modbus.debug)
-              System.out.println("Response:" + response.getHexMessage());
-
-            transport.writeMessage(response);
-
-            count();
-          } catch (ModbusIOException ex) {
-            ex.printStackTrace();
-            continue;
+          //test if Process image exists
+          if (ModbusCoupler.getReference().getProcessImage() == null) {
+            response =
+                request.createExceptionResponse(Modbus.ILLEGAL_FUNCTION_EXCEPTION);
+          } else {
+            response = request.createResponse();
           }
-        }
-        //ensure nice multithreading behaviour on specific platforms
 
-      } while (true);
+          if (Modbus.debug)
+            System.out.println("Request:" + request.getHexMessage());
+          if (Modbus.debug)
+            System.out.println("Response:" + response.getHexMessage());
+
+          transport.writeMessage(response);
+
+        } catch (ModbusIOException ex) {
+          ex.printStackTrace();
+          continue;
+        }
+      } while (m_Listening);
 
     } catch (Exception e) {
       //FIXME: this is a major failure, how do we handle this
@@ -98,34 +114,14 @@ public class ModbusSerialListener {
   }//listen
 
   /**
-   * Sets the listening flag of this <tt>ModbusTCPListener</tt>.
-   *
-   * @param b true if listening (and accepting incoming connections),
-   *        false otherwise.
-   */
-  public void setListening(boolean b) {
-    m_Listening = b;
-  }//setListening
-
-  /**
    * Tests if this <tt>ModbusTCPListener</tt> is listening
    * and accepting incoming connections.
    *
    * @return true if listening (and accepting incoming connections),
-   *          false otherwise.
+   *         false otherwise.
    */
   public boolean isListening() {
     return m_Listening;
   }//isListening
 
-  private void count() {
-    c_RequestCounter++;
-    if (c_RequestCounter == REQUESTS_TOGC) {
-      System.gc();
-      c_RequestCounter = 0;
-    }
-  }//count
-
-  private static final int REQUESTS_TOGC = 15;
-
-}//class ModbusTCPListener
+}//class ModbusSerialListener
