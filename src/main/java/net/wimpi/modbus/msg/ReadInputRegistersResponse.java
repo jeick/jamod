b64cf4b258c31392365dc0027c2da00a59e9a9f6
Charles Hache <chache@brood.ca>
2013-02-28 02:30:09 -0800

    Adding license notices at the top of modified files

--- src/main/java/net/wimpi/modbus/msg/ReadInputRegistersResponse.java
+++ src/main/java/net/wimpi/modbus/msg/ReadInputRegistersResponse.java
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * Original implementation by jamod development team.
+ * This file modified by Charles Hache <chache@brood.ca>
  ***/
 
 package net.wimpi.modbus.msg;
